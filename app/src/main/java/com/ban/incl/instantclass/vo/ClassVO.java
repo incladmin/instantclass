896cc3be6234be1e27cc0bfab9dac240b11091f1
hyunsup1004 <hyunsup1004@naver.com>
2015-07-21 19:56:22 +0900

    ClassVo Update

--- app/src/main/java/com/ban/incl/instantclass/vo/ClassVO.java
+++ app/src/main/java/com/ban/incl/instantclass/vo/ClassVO.java
@@ -5,50 +5,21 @@ package com.ban.incl.instantclass.vo;
  */
 public class ClassVO {
     private String classId;
+    private String addr;
+    private String price;
+
     private String title;
+    private String lessonDate;
     private String date;
     private String startTime;
     private String endTime;
     private String content;
     private String place;
-    private String addr;
-    private String price;
-    private String curr_people;
-    private String max_people;
-    private String end_yn;
-    private String del_yn;
+    private String items;
+    private String minPeople;
+    private String maxPeople;
+    private String account;
 
-    public String getCurr_people() {
-        return curr_people;
-    }
-
-    public void setCurr_people(String curr_people) {
-        this.curr_people = curr_people;
-    }
-
-    public String getMax_people() {
-        return max_people;
-    }
-
-    public void setMax_people(String max_people) {
-        this.max_people = max_people;
-    }
-
-    public String getEnd_yn() {
-        return end_yn;
-    }
-
-    public void setEnd_yn(String end_yn) {
-        this.end_yn = end_yn;
-    }
-
-    public String getDel_yn() {
-        return del_yn;
-    }
-
-    public void setDel_yn(String del_yn) {
-        this.del_yn = del_yn;
-    }
 
     public String getAddr() {
         return addr;
@@ -82,12 +53,12 @@ public class ClassVO {
         this.title = title;
     }
 
-    public String getDate() {
-        return date;
+    public String getLessonDate() {
+        return lessonDate;
     }
 
-    public void setDate(String date) {
-        this.date = date;
+    public void setLessonDate(String lessonDate) {
+        this.lessonDate = lessonDate;
     }
 
     public String getStartTime() {
@@ -121,4 +92,109 @@ public class ClassVO {
     public void setPlace(String place) {
         this.place = place;
     }
+
+    public String getItems() {
+        return items;
+    }
+
+    public void setItems(String items) {
+        this.items = items;
+    }
+
+    public String getMinPeople() {
+        return minPeople;
+    }
+
+    public void setMinPeople(String minPeople) {
+        this.minPeople = minPeople;
+    }
+
+    public String getMaxPeople() {
+        return maxPeople;
+    }
+
+    public void setMaxPeople(String maxPeople) {
+        this.maxPeople = maxPeople;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+
+//    public String getAddr() {
+//        return addr;
+//    }
+//
+//    public void setAddr(String addr) {
+//        this.addr = addr;
+//    }
+//
+//    public String getPrice() {
+//        return price;
+//    }
+//
+//    public void setPrice(String price) {
+//        this.price = price;
+//    }
+//
+//    public String getClassId() {
+//        return classId;
+//    }
+//
+//    public void setClassId(String classId) {
+//        this.classId = classId;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+//
+//    public String getStartTime() {
+//        return startTime;
+//    }
+//
+//    public void setStartTime(String startTime) {
+//        this.startTime = startTime;
+//    }
+//
+//    public String getEndTime() {
+//        return endTime;
+//    }
+//
+//    public void setEndTime(String endTime) {
+//        this.endTime = endTime;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public String getPlace() {
+//        return place;
+//    }
+//
+//    public void setPlace(String place) {
+//        this.place = place;
+//    }
 }
