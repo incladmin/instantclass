d576f8d5f8a95272467ca071363e53936cad2714
scban <bscing@gmail.com>
2015-08-29 16:20:42 +0900

    DB 명 소문자 변경 ( DB 컬럼명 = VO 변수명 )

--- app/src/main/java/com/ban/incl/instantclass/vo/ClassVO.java
+++ app/src/main/java/com/ban/incl/instantclass/vo/ClassVO.java
@@ -4,75 +4,27 @@ package com.ban.incl.instantclass.vo;
  * Created by ots on 2015-05-18.
  */
 public class ClassVO {
-    private String classId;
-    private String addr;
-    private String price;
-
+    private String class_id;
     private String title;
-    private String lessonDate;
-    private String date;
-    private String startTime;
-    private String endTime;
-    private String content;
+    private String addr;
     private String place;
+    private String price;
+    private String content;
+    private String lesson_date;
+    private String start_time;
+    private String end_time;
+    private String min_people;
+    private String max_people;
     private String items;
-    private String minPeople;
-    private String maxPeople;
+    private String bank;
     private String account;
 
-    public String getBank() {
-        return bank;
+    public String getClass_id() {
+        return class_id;
     }
 
-    public void setBank(String bank) {
-        this.bank = bank;
-    }
-
-    private String bank;
-
-    public String getMinPerson() {
-        return minPerson;
-    }
-
-    public void setMinPerson(String minPerson) {
-        this.minPerson = minPerson;
-    }
-
-    private String minPerson;
-
-    public String getMaxPerson() {
-        return maxPerson;
-    }
-
-    public void setMaxPerson(String maxPerson) {
-        this.maxPerson = maxPerson;
-    }
-
-    private String maxPerson;
-
-
-    public String getAddr() {
-        return addr;
-    }
-
-    public void setAddr(String addr) {
-        this.addr = addr;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
-    public String getClassId() {
-        return classId;
-    }
-
-    public void setClassId(String classId) {
-        this.classId = classId;
+    public void setClass_id(String class_id) {
+        this.class_id = class_id;
     }
 
     public String getTitle() {
@@ -83,36 +35,12 @@ public class ClassVO {
         this.title = title;
     }
 
-    public String getLessonDate() {
-        return lessonDate;
+    public String getAddr() {
+        return addr;
     }
 
-    public void setLessonDate(String lessonDate) {
-        this.lessonDate = lessonDate;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
+    public void setAddr(String addr) {
+        this.addr = addr;
     }
 
     public String getPlace() {
@@ -123,6 +51,62 @@ public class ClassVO {
         this.place = place;
     }
 
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getLesson_date() {
+        return lesson_date;
+    }
+
+    public void setLesson_date(String lesson_date) {
+        this.lesson_date = lesson_date;
+    }
+
+    public String getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(String start_time) {
+        this.start_time = start_time;
+    }
+
+    public String getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(String end_time) {
+        this.end_time = end_time;
+    }
+
+    public String getMin_people() {
+        return min_people;
+    }
+
+    public void setMin_people(String min_people) {
+        this.min_people = min_people;
+    }
+
+    public String getMax_people() {
+        return max_people;
+    }
+
+    public void setMax_people(String max_people) {
+        this.max_people = max_people;
+    }
+
     public String getItems() {
         return items;
     }
@@ -131,20 +115,12 @@ public class ClassVO {
         this.items = items;
     }
 
-    public String getMinPeople() {
-        return minPeople;
+    public String getBank() {
+        return bank;
     }
 
-    public void setMinPeople(String minPeople) {
-        this.minPeople = minPeople;
-    }
-
-    public String getMaxPeople() {
-        return maxPeople;
-    }
-
-    public void setMaxPeople(String maxPeople) {
-        this.maxPeople = maxPeople;
+    public void setBank(String bank) {
+        this.bank = bank;
     }
 
     public String getAccount() {
@@ -154,77 +130,4 @@ public class ClassVO {
     public void setAccount(String account) {
         this.account = account;
     }
-
-
-//    public String getAddr() {
-//        return addr;
-//    }
-//
-//    public void setAddr(String addr) {
-//        this.addr = addr;
-//    }
-//
-//    public String getPrice() {
-//        return price;
-//    }
-//
-//    public void setPrice(String price) {
-//        this.price = price;
-//    }
-//
-//    public String getClassId() {
-//        return classId;
-//    }
-//
-//    public void setClassId(String classId) {
-//        this.classId = classId;
-//    }
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-//
-//    public String getStartTime() {
-//        return startTime;
-//    }
-//
-//    public void setStartTime(String startTime) {
-//        this.startTime = startTime;
-//    }
-//
-//    public String getEndTime() {
-//        return endTime;
-//    }
-//
-//    public void setEndTime(String endTime) {
-//        this.endTime = endTime;
-//    }
-//
-//    public String getContent() {
-//        return content;
-//    }
-//
-//    public void setContent(String content) {
-//        this.content = content;
-//    }
-//
-//    public String getPlace() {
-//        return place;
-//    }
-//
-//    public void setPlace(String place) {
-//        this.place = place;
-//    }
 }
